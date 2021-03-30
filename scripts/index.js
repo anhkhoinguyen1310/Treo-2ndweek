5452cae2636f887be38b00294ac172009bec96fa
Loi Van Tran <datloiboi@gmail.com>
2021-03-30 14:37:46 +0700

    Committed

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,30 @@
 console.log('Hello JS!')
 
-// document.getElementById('change-text').innerHTML = 'Hello Change!'
 
-const count = 0
+function changeFirstBox() {
+  console.log('Change My first Box Treo March 30')
+  document.getElementById("change-text").innerHTML = "Treo Change Text!";
+}
+
+setTimeout(changeFirstBox, 3000);
+
+let count = 0
+
+function changeSecondBox() {
+  // count++
+  count = count + 1
+  document.getElementById("change-count").innerHTML = count;
+}
+
+setInterval(changeSecondBox, 1000);
+
+let clickCount = 0
+
+function incrementOnClick() {
+  clickCount++
+  document.getElementById("change-click").innerHTML = clickCount;
+}
+
+document
+  .getElementById("change-click")
+  .addEventListener("click", incrementOnClick);
