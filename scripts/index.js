5b506b33aba3a7e030d9bb5ccd2d1038ff550a75
Nguyen Anh Khoi <nguyenanhkhoi@Nguyens-MacBook-Pro.local>
2021-03-31 12:38:22 +0700

    finishing some parts

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ console.log('Hello JS!')
 
 function changeFirstBox() {
   console.log('Change My first Box Treo March 30')
-  document.getElementById("change-text").innerHTML = "Change the text!!!!";
+  document.getElementById("change-text").innerHTML = "text changed !!!!";
 }
 
 setTimeout(changeFirstBox, 3000);
@@ -16,6 +16,8 @@ function changeSecondBox() {
   document.getElementById("change-count").innerHTML = count;
 }
 
+ 
+
 setInterval(changeSecondBox, 1000);
 
 let clickCount = 0
@@ -24,7 +26,27 @@ function incrementOnClick() {
   clickCount++
   document.getElementById("change-click").innerHTML = clickCount;
 }
+function changetheDate() {
+  document.getElementById("change-to-current-date").innerHTML = changedate;
+
+
+}
+//change color
+const changeColor = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.getElementById("change-color").style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
+}
+setInterval(changeColor, 3000);
+
+//Change hover
+let hoverCount = 0
+document.getElementById("change-hover").addEventListener("mouseover", () => {
+  hoverCount++;
+  document.getElementById("change-hover").innerHTML = hoverCount;
+});
 
 document
   .getElementById("change-click")
   .addEventListener("click", incrementOnClick);
+
